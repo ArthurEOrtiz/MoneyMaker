8a3f399914fd2be7072482230cc9e4a949ddff10
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-08-06 10:49:03 -0600

    Setup Project, divide by gold

--- Program.cs
+++ Program.cs
@@ -6,7 +6,23 @@ namespace MoneyMaker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double bronze = .01;
+            double silver = .05;
+            double gold = .10;
+
+            Console.WriteLine("Please Enter an amount of $, I'll break that down into bronze silver and gold coins");
+            Console.WriteLine("Bronze = $ .01");
+            Console.WriteLine("Silver = $ .05");
+            Console.WriteLine("Gold = $ .10");
+
+            double response = Convert.ToDouble(Console.ReadLine());
+
+            //divisible by ten
+
+            double divisibleByGold = Math.Floor(response / gold);
+            //double remainderOfGold = response % gold;
+            Console.WriteLine(divisibleByGold);
         }
     }
 }
+
